3afb4d7549fa29752022c776ee961a4f0c9adfe4
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2024-12-04 17:41:40 +0800

    feat: implement docker

--- my-portfolio/src/routes/__root.tsx
+++ my-portfolio/src/routes/__root.tsx
@@ -1,11 +1,13 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-
+import { ThemeProvider } from "@/components/ui/theme-provider";
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Outlet />
-      <TanStackRouterDevtools />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Outlet />
+        <TanStackRouterDevtools />
+      </ThemeProvider>
     </>
   ),
 });
