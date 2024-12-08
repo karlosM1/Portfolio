e01f5d62cdf44e4f71c065b464233951649bbea9
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2024-12-08 11:01:11 +0800

    fix: skills icons

--- my-portfolio/src/components/technologies.tsx
+++ my-portfolio/src/components/technologies.tsx
@@ -1,23 +1,31 @@
 import { cn } from "@/lib/utils";
 import Marquee from "./ui/marquee";
 import { reviews } from "../data/resume";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+import { Button } from "./ui/button";
 
 const firstRow = reviews.slice(0, reviews.length / 2);
 const secondRow = reviews.slice(reviews.length / 2);
 
 const ReviewCard = ({ img, name }: { img: string; name: string }) => {
   return (
-    <figure
-      className={cn(
-        "relative w-34 cursor-pointer overflow-hidden rounded-full border p-4",
-        // light styles
-        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-        // dark styles
-        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
-      )}
-    >
-      <div className="flex flex-row items-center gap-2">
-        <img src={img} alt={name} className="h-12 w-12 rounded-full" />
+    <figure className={cn("relative cursor-pointer w-full")}>
+      <div className="flex flex-row items-center gap-3">
+        <TooltipProvider delayDuration={0}>
+          <Tooltip>
+            <TooltipTrigger>
+              <img src={img} alt={name} className="h-12 w-12" />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{name}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
     </figure>
   );
@@ -31,7 +39,7 @@ export function Technologies() {
           <ReviewCard key={review.name} {...review} />
         ))}
       </Marquee>
-      <Marquee reverse pauseOnHover className="[--duration:20s]">
+      <Marquee reverse pauseOnHover className="[--duration:15s]">
         {secondRow.map((review) => (
           <ReviewCard key={review.name} {...review} />
         ))}
