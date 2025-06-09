46b57f42d8ed6b81874a84f419a75beb3fb81685
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2025-06-10 00:53:13 +0800

    fix: remove TanStackRouterDevtools from root route for cleaner component structure

--- my-portfolio/src/routes/__root.tsx
+++ my-portfolio/src/routes/__root.tsx
@@ -1,12 +1,10 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { ThemeProvider } from "@/components/theme-provider";
 export const Route = createRootRoute({
   component: () => (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <Outlet />
-        <TanStackRouterDevtools />
       </ThemeProvider>
     </>
   ),
