6183ac0c22c6e8e971676fd2c2812d63c3572ff8
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2025-06-10 00:41:49 +0800

    fix: update description to replace 'Twitter' with 'X'

--- my-portfolio/src/data/resume.tsx
+++ my-portfolio/src/data/resume.tsx
@@ -6,7 +6,7 @@ export const DATA = {
   initials: "KMS",
   location: "Quezon City, Philippines",
   description:
-    "Full Stack Engineer who’s all about building cool stuff from front to back. I love tackling challenges and making things work. Catch me on Twitter for some tech talk!",
+    "Full Stack Engineer who’s all about building cool stuff from front to back. I love tackling challenges and making things work. Catch me on X for some tech talk!",
   summary:
     "I graduated in 2025 with a computer science degree from Lyceum of thePhilippines University (LPU). Along the way, I leveled up my codingskills, joined coding challenges, and did a few internships with localtech companies. I’m all about building cool stuff and ready to divedeeper into the tech world!",
   avatarUrl: images.profilepicture,
